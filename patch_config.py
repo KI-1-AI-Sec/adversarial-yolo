2fc547edc3bb3caf156e085966e29bd9a3520e14
mkrupczak3 <matthew@krupczak.org>
2023-10-30 20:27:40 -0400

    trying to train with fixed 640 model input size

--- patch_config.py
+++ patch_config.py
@@ -18,6 +18,8 @@ class BaseConfig(object):
         #Convenience Additions
         self.img_height = 512
         self.img_width = 512
+        self.input_width = 640
+        self.input_height = 640
         self.model_path = 'best.pt'
         self.nps_weight = 0.01
         self.tv_weight = 2.5
